fdb08c0a1f3817d36b91b19c04a98b8303412668
Patryk <master_sergeant98@interia.pl>
2017-09-25 02:14:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var withButtonClass = document.getElementsByClassName('spike');
+
+console.log(withButtonClass);
+
+for (i=0; i<withButtonClass.length; i++){
+	console.log(withButtonClass[i].innerText)       
+}
